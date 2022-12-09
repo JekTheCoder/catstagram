40ae677985b25685b0cb6974fc80ecb53fc98569
Jeshua <hinostrozajeshua@gmail.com>
2022-12-09 15:50:30 -0500

    create: basic page structure

---
+++ src/lib/components/Main.svelte
@@ -0,0 +1,11 @@
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		max-width: 36rem;
+		margin: auto;
+		padding: 0 2rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Header from '$lib/components/Header.svelte';
+	import Main from '$lib/components/Main.svelte';
+	import Timeline from '$lib/components/Timeline.svelte';
+</script>
+
+<Header />
+<Main>
+	<Timeline />
+</Main>
+
+<style>
+	:global(:root) {
+		--color-primary: 0, 0, 0;
+		--color-accent: 0, 0, 0;
+		--color-warn: 255, 20, 20;
+		--color-bg: 250, 250, 250;
+	}
+
+	:global(body) {
+		background-color: rgb(var(--color-bg));
+	}
+
+	:global(*) {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+</style>
